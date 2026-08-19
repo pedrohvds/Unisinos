9f49cbd830183dc0f38e34f730c3d2d7d67ddca9
pedrohvds <pedro2006vdesouza@gmail.com>
2026-08-19 20:13:45 -0300

    Add files via upload

---
+++ Exercicio aula 19.08.2026.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    double l, r;
+    printf("Digite a quantidade de tinta em litros: ");
+    scanf("%lf", &l);
+    printf("Digite o valor de cada litro em reais: ");
+    scanf("%lf", &r);
+    double altura, largura;
+    printf("Digite a altura da parede (em METROS): ");
+    scanf("%lf", &altura);
+    printf("Digite a largura da parede (em METROS): ");
+    scanf("%lf", &largura);
+    double tinta = (altura*largura*2) / 5;
+    double custo = tinta*r;
+    printf("Quantidade necessária para dar 2 de mão na parede: L%lf", tinta);
+    printf("\nCusto total: R$%lf", custo);
+    return 0;
+}
